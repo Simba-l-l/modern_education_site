af4fd4d5a742040c7a826e26758a8c0dba0434ca
simba-l-l <prokhorovm.v@mail.ru>
2022-06-23 22:10:21 +0300

    сделали старые нароботки

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import Nav from './components/navbar.jsx'
+import Content  from './components/content';
 
 function App() {
   return (
     <div className="App">
         {Nav()}
+        {Content()}
     </div>
   );
 }
